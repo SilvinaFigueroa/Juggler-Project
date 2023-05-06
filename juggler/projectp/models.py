3a8ea49319f7126f38dcc6cb6dade380d1729614
SilvinaFigueroa <86253843+SilvinaFigueroa@users.noreply.github.com>
2023-05-05 20:00:58 -0700

    Submission

---
+++ juggler/projectp/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+
+
+# Create your models here.
+
+class Project(models.Model):
+    name = models.CharField(max_length=100,
+                            help_text="Project Name")
+
+    def __str(self):
+        return self.name
+
+
+class Task(models.Model):
+    title = models.CharField(max_length=100,
+                             help_text="Task Title")
+    description = models.TextField(max_length=300,
+                                   verbose_name="Describe Task")
+    project = models.ForeignKey(Project, on_delete=models.CASCADE)
+
+    def __str(self):
+        return self.title
